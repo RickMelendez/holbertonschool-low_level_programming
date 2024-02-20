760a721af0e72d6a78ca563456fd8389f53a4649
RickMelendez <rickmelendez001@gmail.com>
2024-02-20 15:00:24 -0400

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main- Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        int ch, low, e, q;
+
+	e= 'e';
+	q= 'q';
+
+        for (ch = 'a'; ch <= 'z'; ch++)
+                putchar(ch);
+        putchar('\n');
+        return (0);
+}
