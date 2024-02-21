81c68d8b0be808312f233142c7480d3b747185a4
RickMelendez <rickmelendez001@gmail.com>
2024-02-20 22:39:15 -0400

    4-print_alphabt

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
-
 /**
- *main- Entry point
+ *main - Entry point
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-        int ch, low, e, q;
+	char i = 'a';
 
-	e= 'e';
-	q= 'q';
-
-        for (ch = 'a'; ch <= 'z'; ch++)
-                putchar(ch);
-        putchar('\n');
-        return (0);
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'e' && i != 'q')
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
