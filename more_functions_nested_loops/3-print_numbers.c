6acfea01e61ad0206847609625ad40c04cfb67cf
RickMelendez <rickmelendez001@gmail.com>
2024-02-22 13:55:49 -0400

    3-print_numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_numbers- prints the numbers
+ *
+ * Return: value
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+
+}
+
