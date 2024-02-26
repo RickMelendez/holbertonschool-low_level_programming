c7e8f4531b5480a0a2ba251edf4d5cc96145142c
RickMelendez <rickmelendez001@gmail.com>
2024-02-26 19:02:38 -0400

    1-swap

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,13 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *swap_int- swaps the values of two integers
- *@a: integer pointer
- *@b: integer pointer
+ *swap_int- swaps the values of two integers.
+ *@a: first integer to be swapped.
+ *@b: second integer to be swapped.
  *
  * Return: nothing
  */
 void swap_int(int *a, int *b)
 {
-	
-
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
