954daae6ecc5e5e7c104e50fa7134c98227c795e
RickMelendez <rickmelendez001@gmail.com>
2024-02-26 09:27:06 -0400

    8-print_square.c

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *print_square- prins a square
+ *
+ *@size: size of the square
+ */
+void print_square(int size)
+{
+	int t, o;
+
+	if (size > 0)
+	{
+		for (t = 0; t < size; t++)
+		{
+			for (o = 0; o < size; o++)
+			{
+				_putchar('#');
+			}
+				_putchar('\n');
+		}
+	}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}
