6148f3c9f42176c3123dd26d27e61e90da5a9c07
RickMelendez <rickmelendez001@gmail.com>
2024-02-26 09:24:57 -0400

    7-print_diagonal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *print_diagonal- it prints a line
+ *@n: number of times
+ *
+ */
+void print_diagonal(int n)
+{
+	int t, s;
+
+	for (t = 0; t < n; t++)
+	{
+		if (n > 0)
+		{
+			for (s = 0; s < t; s++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}
