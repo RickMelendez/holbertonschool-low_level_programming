224964b8ac49f36aab2a7a9b77f9d8c3d72cad23
RickMelendez <rickmelendez001@gmail.com>
2024-03-01 00:05:09 -0400

    7-print_chessboard

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,2 +1,21 @@
 #include "main.h"
-#include <stdio.h>
+/**
+ * print_chessboard- print the chessboard
+ * @a: array of pieces
+ *
+ *Return: Nothing.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int h, d;
+
+	for (h = 0; h < 8; h++)
+	{
+		for (d = 0; d < 8; d++)
+		{
+			_putchar(a[h][d]);
+		}
+
+		_putchar('\n');
+	}
+}
