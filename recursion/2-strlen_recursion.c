3e6c6537abae5b85dd66c1465579c0c422e2d810
RickMelendez <rickmelendez001@gmail.com>
2024-03-04 23:47:16 -0400

    3-factorial

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,3 +1,15 @@
 #include "main.h"
-#include <stdio.h>
-
+/**
+ *_strlen_recursion- calculate lenght
+ *@s: string
+ *Return: int value
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	s++;
+	return (_strlen_recursion(s) + 1);
+}
